66b18a6b29cb1948b79ce5d73ed192385fe45aef
roman-bg <61371346+roman-bg@users.noreply.github.com>
2026-03-31 17:03:06 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,51 +1,24 @@
 const CACHE_NAME = 'bumblebee-id-v3';
-const OFFLINE_URL = 'offline.html';
-
 const urlsToCache = [
-    './',
-    './index.html',
-    './offline.html',
-    './manifest.json',
-    './icons/icon-192x192.png',
-    './icons/icon-512x512.png'
+  './',
+  'index.html',
+  'manifest.json',
+  'icon-48x48.png',
+  'icon-72x72.png',
+  'icon-96x96.png',
+  'icon-144x144.png',
+  'icon-192x192.png',
+  'icon-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(urlsToCache);
-        })
-    );
-    self.skipWaiting(); // Форсира активацията
-});
-
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-    self.clients.claim();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    if (event.request.mode === 'navigate') {
-        event.respondWith(
-            fetch(event.request).catch(() => {
-                return caches.match(OFFLINE_URL);
-            })
-        );
-    } else {
-        event.respondWith(
-            caches.match(event.request).then((response) => {
-                return response || fetch(event.request);
-            })
-        );
-    }
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
