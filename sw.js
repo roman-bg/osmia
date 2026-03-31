0965352e2eb6b5459c5e56d1536ce0895191b6e7
roman-bg <61371346+roman-bg@users.noreply.github.com>
2026-03-31 15:31:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,14 +10,11 @@ const urlsToCache = [
 '/index.html',
 '/offline.html',
 '/manifest.json',
+'/icons/icon-48x48.png',
 '/icons/icon-72x72.png',
 '/icons/icon-96x96.png',
-'/icons/icon-128x128.png',
 '/icons/icon-144x144.png',
-'/icons/icon-152x152.png',
 '/icons/icon-192x192.png',
-'/icons/icon-256x256.png',
-'/icons/icon-384x384.png',
 '/icons/icon-512x512.png'
 ];
 
